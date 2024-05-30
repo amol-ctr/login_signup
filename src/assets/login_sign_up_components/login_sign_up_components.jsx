f2163bd95fa3b316b255482793f961eb5ce3641d
AMOL MITTAL <58140879+amol-ctr@users.noreply.github.com>
2024-05-30 23:52:15 +0530

    Update login_sign_up_components.jsx

--- src/assets/login_sign_up_components/login_sign_up_components.jsx
+++ src/assets/login_sign_up_components/login_sign_up_components.jsx
@@ -10,66 +10,84 @@ import './login_sign_up_components.css'
 
 function Login_sign_up_components() {
 
-  const [action, setAction] = useState("Sign Up")
+  const [action, setAction] = useState("LOGIN")
   return (
     <div class='container2'>
       <div className=' header'>
-        <div className=' text'>Tensor {action}</div>
-        <div className=' underline'></div>
+        <div className=' text'>{action}</div>
+        {/* <br /> */}
+        {/* <div className=' underline'></div> */}
       </div>
 
-      <div className='submit-container'>
+      {/* <div className='submit-container'>
         <button type='submit' className={action == "Login" ? "submit" : "submit gray"} onClick={() => setAction("Login")}>Login</button>
         <button type='submit' className={action == "Sign Up" ? "submit" : "submit gray"} onClick={() => setAction("Sign Up")}>Sign Up</button>
-      </div>
+      </div> */}
 
       <form id="registerForm" action="/registration" method="post" data-sb-form-api-token="API_TOKEN">
-
         <div className='inputs'>
 
-          {action == "Login" ? <div></div> :
+          <h4 className=' ml-20'>Username</h4>
+          <div className='input'>
+            <img src={person} alt="" />
+            <input type="text" placeholder='Type your username' required />
+            <div class="invalid-feedback" data-sb-feedback="Username:required">A Username is required.</div>
+          </div>
 
-            <div className='input'>
-              <img src={person} alt="" />
-              <input type="text" placeholder='Name' required />
-              <div class="invalid-feedback" data-sb-feedback="Username:required">A Username is required.</div>
+          {action == "LOGIN" ? <div></div> :
+            <div>
+              <h4 className=' ml-20'>Email</h4>
+              <div className='input'>
+                <img src={email} alt="" />
+                <input type="email" placeholder='Email Id' required />
+                <div class="invalid-feedback" data-sb-feedback="email:required">An email is required.</div>
+                <div class="invalid-feedback" data-sb-feedback="email:email">Email is not valid.</div>
+              </div>
             </div>
           }
-          <div className='input'>
-            <img src={email} alt="" />
-            <input type="email" placeholder='Email Id' required />
-            <div class="invalid-feedback" data-sb-feedback="email:required">An email is required.</div>
-            <div class="invalid-feedback" data-sb-feedback="email:email">Email is not valid.</div>
-          </div>
+
+          <h4 className=' ml-20'>Password</h4>
           <div className='input'>
             <img src={password} alt="" />
-            <input type="password" placeholder='Password' required />
+            <input type="password" placeholder='Type your password' required />
             <div class="invalid-feedback" data-sb-feedback="password:required">A password is required.</div>
           </div>
         </div>
 
-        {action == "Sign Up" ? <div className='forgot-password'></div> :
+        {action == "SIGN UP" ? <div className='forgot-password'></div> :
           <div className='forgot-password'><a href="#">Forgot Password?</a></div>
         }
 
-        <button type='submit' className='submit2'>Submit</button>
-        {/* <br/> */}
+        <button type='submit' className='submit2'>{action}</button>
+        <br/>
 
-        <p align="center">or</p>
-        <p align="center">{action} using:</p>
+        {action=="SIGN UP" ? <div>
 
-        <span class='social_media_icons'>
+        <p align="center">Or {action=="LOGIN"?"Sign up":"Login"} using:</p>
+        <p align="center" onClick={()=>setAction("LOGIN")}>LOGIN</p>
 
-          <a href="https://www.twitter.com">
-            <img src={twitter} width=" 40px" height="40px" alt="" />
-          </a>
-          <a href="https://www.facebook.com">
-            <img src={facebook} width=" 40px" height="40px" alt="" />
-          </a>
-          <a href="https://www.github.com">
-            <img src={github} width=" 40px" height="40px" alt="" />
-          </a>
-        </span>
+        </div>:
+        <div>
+          <p align="center">Or {action=="LOGIN"?"Sign up":"Login"} using:</p>
+
+          <span class='social_media_icons'>
+
+            <a href="https://www.twitter.com">
+              <img src={twitter} width=" 40px" height="40px" alt="" />
+            </a>
+            <a href="https://www.facebook.com">
+              <img src={facebook} width=" 40px" height="40px" alt="" />
+            </a>
+            <a href="https://www.github.com">
+              <img src={github} width=" 40px" height="40px" alt="" />
+            </a>
+          </span>
+
+          <br/>
+          <p align="center">Or {action=="LOGIN"?"Sign up":"Login"} using:</p>
+          <p align="center" onClick={()=>setAction("SIGN UP")}>SIGN UP</p>
+        </div>
+        }
       </form>
 
     </div>
