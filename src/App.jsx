a37e9d5de6dfc9f418a649fcd7131d586fd078b7
AMOL MITTAL <58140879+amol-ctr@users.noreply.github.com>
2024-07-07 07:21:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
 import Login_sign_up_components from './assets/login_sign_up_components/login_sign_up_components'
+import Forgot_password_page from './assets/login_sign_up_components/forgot_password_page'
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 
 function App() {
 
   return (
     <>
       <div>
-        <Login_sign_up_components/>
+        {/* <Login_sign_up_components /> */}
+        <Routes>
+          <Route path="*" element={<Login_sign_up_components />} /> 
+          <Route path="/forgot_password_page" element={<Forgot_password_page />} />
+        </Routes>
       </div>
-        
+
     </>
   )
 }
